762b250f7ef2abdb9a9b38348c480b7eeb15e1d9
shaylevin89 <shayleevin89@gmail.com>
2021-03-25 16:37:37 +0200

    write message

--- DB.py
+++ DB.py
@@ -6,12 +6,19 @@ LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 DATABASE_URL = os.getenv('DATABASE_URL', "dbname=root user=root password=root")
-logging.info(DATABASE_URL)
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-# conn = psycopg2.connect("dbname=root user=root password=root", sslmode='require')
 cur = conn.cursor()
 
-users = cur.execute("SELECT password FROM users")
-rows = cur.fetchall()
-# print(rows)
+
+def insert_message(sender, receiver, subject, msg_data, created_at):
+    try:
+        cur.execute(f"""INSERT INTO messages (sender, receiver, subject, msg_data, created_at) 
+        VALUES('{sender}', '{receiver}', '{subject}', '{msg_data}', '{created_at}');""")
+        conn.commit()
+        return True
+    except Exception as e:
+        cur.execute('rollback;')
+        print(e)
+        return False
+
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import logging
 import DB
 
@@ -8,18 +8,42 @@ logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    return str(DB.rows)
+@app.route('/register', methods=['POST'])
+def register():
+    return ''
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    return ''
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    return ''
 """
-- Write message
+- Write message  V
 - Get all messages for a specific user
 - Get all unread messages for a specific user
 - Read message (return one message)
 - Delete message (as owner or as receiver)
 """
+
+
+@app.route("/message", methods=['POST'])
+def write_message():
+    try:
+        msg = request.get_json(force=True)
+        # TODO get sender name or just validate
+        if DB.insert_message(
+                msg.get('sender'), msg.get('receiver'), msg.get('subject'), msg.get('msg_data'), msg.get('created_at')):
+            return {"message": "Message created"}, 200
+        else:
+            return {"error": "Wrong message keys or values"}, 400
+    except Exception as e:
+        logging.error(e)
+        return {"error": "Wrong message format. only JSON accepted"}, 400
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 3489)
