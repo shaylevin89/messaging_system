a1a8349366afef024e643fbfeeb6d3f5c8d193a7
shaylevin89 <shayleevin89@gmail.com>
2021-03-25 11:58:39 +0200

    hello

--- DB.py
+++ DB.py
@@ -1,11 +1,11 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
+DATABASE_URL = os.getenv('DATABASE_URL', "dbname=root user=root password=root")
+print(DATABASE_URL)
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# conn = psycopg2.connect("dbname=root user=root password=root")
+# conn = psycopg2.connect("dbname=root user=root password=root", sslmode='require')
 cur = conn.cursor()
 
 users = cur.execute("SELECT password FROM users")
