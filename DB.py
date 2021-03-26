6b805e7bd12a0d1fed4a77493ced51742d26a357
shaylevin89 <shayleevin89@gmail.com>
2021-03-26 15:50:38 +0300

    system work locally

--- DB.py
+++ DB.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import logging
+from werkzeug.security import generate_password_hash, check_password_hash
 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
@@ -18,7 +19,78 @@ def insert_message(sender, receiver, subject, msg_data, created_at):
         return True
     except Exception as e:
         cur.execute('rollback;')
-        print(e)
+        logging.error(e)
+        return False
+
+
+def get_messeges(username, unread=None):
+    if unread:
+        cur.execute(
+            f"""select * from messages where unread = 't' and (sender = '{username}' or receiver = '{username}');""")
+    else:
+        cur.execute(f"""select * from messages where sender = '{username}' or receiver = '{username}';""")  #
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        return False
+    elif len(rows) == 1:
+        message_id_tup = '('+str(rows[0][0])+')'
+    else:
+        message_id_tup = tuple(row[0] for row in rows)
+    update_unread_to_read(message_id_tup)
+    return rows
+
+
+def get_message(msg_id, username):
+    try:
+        cur.execute(f"""select * from messages where msg_id = {msg_id} and 
+        (sender = '{username}' or receiver = '{username}');""")
+        row = cur.fetchone()
+        return row
+    except Exception as e:
+        logging.error(e)
+        cur.execute('rollback;')
+        return False
+
+
+def delete_message(msg_id, username):
+    cur.execute(f"""delete from messages where msg_id = {msg_id} and 
+        (sender = '{username}' or receiver = '{username}');""")
+    conn.commit()
+
+
+def update_unread_to_read(msg_id_tup):
+    try:
+        cur.execute(f"""update messages set unread = 'f' where msg_id in {msg_id_tup};""")
+        conn.commit()
+        return True
+    except Exception as e:
+        logging.error(e)
+        cur.execute('rollback;')
+        return False
+
+
+def create_user(username, password):
+    try:
+        hashed_password = generate_password_hash(password, 'sha256')
+        cur.execute(f"""INSERT INTO users (username, password) VALUES('{username}', '{hashed_password}');""")
+        conn.commit()
+        return True
+    except Exception as e:
+        logging.error(e)
+        cur.execute('rollback;')
+        return False
+
+
+def check_user_credentials(username, password):
+    try:
+        cur.execute(f"""select * from users where username = '{username}';""")
+        user = cur.fetchone()
+        if user and check_password_hash(user[2], password):
+            return True
+        return False
+    except Exception as e:
+        logging.error(e)
+        cur.execute('rollback;')
         return False
 
 
--- app.py
+++ app.py
@@ -1,48 +1,124 @@
 from flask import Flask, request, jsonify
 import logging
 import DB
+from functools import wraps
+import jwt
+import datetime
 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 app = Flask(__name__)
+app.config['secret'] = 'shaylevin'
+
+
+class Message:
+    def __init__(self, row_message):
+        self.row_message = row_message
+        self.json_message = dict(msg_id=None, sender=None, receiver=None, subject=None, msg_data=None, created_at=None)
+
+    def to_json_message(self):
+        self.json_message['msg_id'] = self.row_message[0]
+        self.json_message['sender'] = self.row_message[1]
+        self.json_message['receiver'] = self.row_message[2]
+        self.json_message['subject'] = self.row_message[3]
+        self.json_message['msg_data'] = self.row_message[4]
+        self.json_message['created_at'] = self.row_message[5]
+        return self.json_message
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    return ''
+    credentials = request.get_json(force=True)
+    if 'username' not in credentials or 'password' not in credentials:
+        return {"error": "Credentials missing"}, 400
+    if DB.create_user(credentials['username'], credentials['password']):
+        return {"message": "User created"}, 200
+    return {"error": "Username not available"}
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    return ''
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return {"error": "Authentication required"}, 400
+    if not DB.check_user_credentials(auth.username, auth.password):
+        return {"error": "Authentication failed"}, 400
+    token = jwt.encode({'username': auth.username,
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=20)},
+                       app.config['secret'], 'HS256')
+    return jsonify({'token': token})
 
 
-@app.route('/logout', methods=['POST'])
-def logout():
-    return ''
-"""
-- Write message  V
-- Get all messages for a specific user
-- Get all unread messages for a specific user
-- Read message (return one message)
-- Delete message (as owner or as receiver)
-"""
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Auth-token' in request.headers:
+            token = request.headers['Auth-token']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['secret'], 'HS256')
+            username = data['username']
+        except Exception as e:
+            logging.error(e)
+            return jsonify({'message': 'Token is invalid, please login'}), 401
+        return f(username, *args, **kwargs)
+    return decorated
 
 
 @app.route("/message", methods=['POST'])
-def write_message():
+@token_required
+def write_message(username):
     try:
         msg = request.get_json(force=True)
-        # TODO get sender name or just validate
         if DB.insert_message(
-                msg.get('sender'), msg.get('receiver'), msg.get('subject'), msg.get('msg_data'), msg.get('created_at')):
-            return {"message": "Message created"}, 200
+                username, msg.get('receiver'), msg.get('subject'), msg.get('msg_data'), msg.get('created_at')):
+            return {"message": "Message created"}, 201
         else:
             return {"error": "Wrong message keys or values"}, 400
     except Exception as e:
         logging.error(e)
         return {"error": "Wrong message format. only JSON accepted"}, 400
+
+
+@app.route("/message/<msg_id>", methods=['GET'])
+@token_required
+def read_message(username, msg_id):
+    if msg_id.isdigit():
+        row_message = DB.get_message(msg_id, username)
+        if row_message:
+            json_message = Message(row_message).to_json_message()
+            return jsonify({'message': json_message}), 200
+        return {"error": "Msg_id not found"}, 400
+    return {"error": "Msg_id not valid"}, 400
+
+
+@app.route("/unread_messages", methods=['GET'])
+@app.route("/messages", methods=['GET'])
+@token_required
+def read_messages(username):
+    path = request.path
+    unread = None
+    if path == '/unread_messages':
+        unread = True
+    messages = DB.get_messeges(username, unread)
+    if messages:
+        json_messages = []
+        for message in messages:
+            json_messages.append(Message(message).to_json_message())
+        return jsonify({'messages': json_messages}), 200
+    return {"message": "No messages for current user"}, 200
+
+
+@app.route('/message/<msg_id>', methods=['DELETE'])
+@token_required
+def delete_message(username, msg_id):
+    if msg_id.isdigit():
+        DB.delete_message(msg_id, username)
+        return {"message": f"Message {msg_id} no longer exist"}, 200
+    return {"error": "Msg_id not valid"}, 400
 
 
 if __name__ == '__main__':
